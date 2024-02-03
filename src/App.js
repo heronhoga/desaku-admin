27d6ee14bb35d7a0ec7449b5972141d24fdda959
heronhoga <nozzeravixer@gmail.com>
2024-02-03 23:20:06 +0700

    feat:add listrik func for monitoring admin

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Welcome from "./views/Welcome";
 import Login from "./views/Login";
 import Home from "./views/Home";
+import Listrik from "./views/Listrik";
 import { Route, Routes } from "react-router-dom";
 import Warga from "./views/Warga";
 import EditWarga from "./views/EditWarga";
+
 function App() {
   return (
     <div className="container">
@@ -14,6 +16,7 @@ function App() {
         <Route path="/warga" element={<Warga />} />
         <Route path="/warga" element={<Warga />} />
         <Route path="/editwarga/:id" element={<EditWarga />} />
+        <Route path="/listrik" element={<Listrik />} />
       </Routes>
     </div>
   );
---
+++ src/views/Listrik.js
@@ -0,0 +1,157 @@
+import React, { useState, useEffect } from "react";
+import { Navbar } from "../components/Navbar";
+import { useNavigate } from "react-router-dom";
+import Table from "../components/Table";
+import "../views/styles/Listrik.css";
+
+function Listrik() {
+  const [showAddBillButton, setShowAddBillButton] = useState(false);
+  const headers = [
+    "Total Tagihan Listrik",
+    "Tanggal Tagihan",
+    "Status",
+    "Nama",
+    "Alamat",
+  ];
+  const [searchText, setSearchText] = useState("");
+  const [selectedMonth, setSelectedMonth] = useState(getCurrentMonth());
+  const [rows, setRows] = useState([]);
+  const [filteredRows, setFilteredRows] = useState([]);
+  const navigate = useNavigate();
+
+  function getCurrentMonth() {
+    const month = new Date().getMonth() + 1;
+    return month < 10 ? `0${month}` : `${month}`;
+  }
+
+  useEffect(() => {
+    const lowercasedSearchText = searchText.toLowerCase();
+    setFilteredRows(
+      rows.filter(
+        (row) =>
+          row[0].toLowerCase().includes(lowercasedSearchText) ||
+          row[3].toLowerCase().includes(lowercasedSearchText)
+      )
+    );
+  }, [searchText, rows]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:8080/admin/listrik/tagihan/${selectedMonth}`,
+          {
+            credentials: "include",
+          }
+        );
+
+        if (!response.ok) {
+          if (response.status === 404) {
+            setRows([]);
+            setFilteredRows([]);
+            setShowAddBillButton(true);
+            return;
+          }
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const jsonData = await response.json();
+
+        const formattedData = jsonData.data.map((item) => [
+          new Intl.NumberFormat("id-ID", {
+            style: "currency",
+            currency: "IDR",
+          }).format(item.total_tagihan_listrik),
+          item.tanggal_tagihan,
+          item.status,
+          item.nama,
+          item.alamat,
+        ]);
+
+        setRows(formattedData);
+        setFilteredRows(formattedData);
+        setShowAddBillButton(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [selectedMonth]);
+
+  const handleAddBillClick = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:8080/admin/listrik/tagihan/${selectedMonth}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+
+          },
+          credentials: "include",
+
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const jsonData = await response.json();
+
+      console.log("Bill created successfully:", jsonData);
+
+      // Refresh the page
+      window.location.reload(true);
+    } catch (error) {
+      console.error("Error creating bill:", error);
+    }
+  };
+
+  return (
+    <div className="wargaNav">
+      <Navbar />
+
+      <div className="searchBar">
+        <input
+          type="text"
+          placeholder="Cari warga (Nama).."
+          className="searchInput"
+          value={searchText}
+          onChange={(event) => setSearchText(event.target.value)}
+        />
+        <label htmlFor="monthSelect">Pilih bulan tagihan</label>
+        <select
+          id="monthSelect"
+          value={selectedMonth}
+          onChange={(event) => setSelectedMonth(event.target.value)}
+        >
+          {Array.from({ length: 12 }, (_, index) => {
+            const monthNumber = (index + 1).toString().padStart(2, "0");
+            return (
+              <option key={monthNumber} value={monthNumber}>
+                {new Date(2000, index).toLocaleString("en-US", {
+                  month: "long",
+                })}
+              </option>
+            );
+          })}
+        </select>
+
+        <br></br>
+        {showAddBillButton && (
+          <div className="addBillButtonContainer">
+            <button onClick={handleAddBillClick} className="btn btn-success">
+              Tambahkan tagihan bulan ini
+            </button>
+          </div>
+        )}
+      </div>
+
+      <Table headers={headers} rows={filteredRows} />
+    </div>
+  );
+}
+
+export default Listrik;
