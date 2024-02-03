9df0088377a10c3ccab207a65dd130fe7bbf881e
heronhoga <nozzeravixer@gmail.com>
2024-02-03 17:06:01 +0700

    feat:add home page layout

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,12 +1,56 @@
 import React from 'react'
 import { Navbar } from '../components/Navbar'
 import '../views/styles/Home.css';
+import { useEffect, useState } from 'react';
 
 function Home() {
+
+  const [total, setTotal] = useState(0);
+
+useEffect(() => {
+  const fetchData = async (url) => {
+    const response = await fetch(url, {
+      credentials: 'include',
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const contentType = response.headers.get("content-type");
+    if (contentType && contentType.indexOf("application/json") !== -1) {
+      try {
+        const data = await response.json();
+        console.log(data.data);
+        
+        setTotal(data.data.length);
+
+        return data.data;
+      } catch (error) {
+        console.error("Error parsing JSON!", error);
+      }
+    } else {
+      console.error("This is not JSON!");
+    }
+  };
+
+  fetchData('http://localhost:8080/admin/warga', {
+    credentials: 'include',
+  });
+}, []);
+
   return (
-    <div className="homeNav"><Navbar/></div>
-    
-  )
+    <div className="homeNav">
+      <Navbar/>
+      <br></br>
+      <h1>Selamat datang di</h1>
+      <h1>DESAKU WEBSITE - ADMIN</h1>
+      <br></br>
+      <h2>Saat ini sudah ada {total} data warga yang terdaftar</h2>
+      <h2>di sistem Desaku</h2>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
+
